bd0b3816d033445fac1ef5873305dfaf82db6d20
unknown <miguel317.angelps@gmail.com>
2024-12-18 09:43:19 +0100

    Actualizacion 6.6 añadido try catch en funcion final

---
+++ Genero.java
@@ -0,0 +1,17 @@
+package euskalcinesprograma;
+
+
+import java.util.ArrayList;
+
+public class Genero {
+    private String nombre; //Tipo de genero 
+
+    Genero(String nombre) {
+        this.nombre = nombre;
+    }
+    //Getter nombre del genero
+    public String getNombre() {
+        return nombre;
+    }
+
+}
